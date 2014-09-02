162d99591a36b53a48764e7f743847ecff77f75f
cosmospostman <buck68@gmail.com>
2014-09-01 22:14:16 -0700

    Use vanilla Service for livestream; add binder

--- src/org/kfjc/droid/LiveStreamService.java
+++ src/org/kfjc/droid/LiveStreamService.java
@@ -2,67 +2,84 @@ package org.kfjc.droid;
 
 import java.io.IOException;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.widget.Toast;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
 
-public class LiveStreamService extends IntentService {
+public class LiveStreamService extends Service implements
+		MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+		MediaPlayer.OnCompletionListener {
 
 	private static final String AAC_HI = "http://netcast6.kfjc.org:80/";
-
-	MediaPlayer mPlayer;
-
-	public LiveStreamService(String name) {
-		super(name);
-		mPlayer = new MediaPlayer();
-		mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-	}
-	
-	public LiveStreamService() {
-		this("Default lss");
-	}
+	private MediaPlayer mPlayer;
+	private final IBinder liveStreamBinder = new LiveStreamBinder();
 
 	@Override
-	protected void onHandleIntent(Intent arg0) {
+	public void onCreate() {
+		super.onCreate();
+		mPlayer = new MediaPlayer();
+		initPlayer();
+	}
 
-		// Set source
+	private void initPlayer() {
+		// TODO: Add permissions to manifest.
+		mPlayer.setWakeMode(getApplicationContext(),
+				PowerManager.PARTIAL_WAKE_LOCK);
+		mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mPlayer.setOnPreparedListener(this);
 		try {
 			mPlayer.setDataSource(AAC_HI);
 		} catch (IllegalArgumentException e) {
-			Toast.makeText(getApplicationContext(),
-					"You might not set the URI correctly!", Toast.LENGTH_LONG)
-					.show();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (SecurityException e) {
-			Toast.makeText(getApplicationContext(),
-					"You might not set the URI correctly!", Toast.LENGTH_LONG)
-					.show();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IllegalStateException e) {
-			Toast.makeText(getApplicationContext(),
-					"You might not set the URI correctly!", Toast.LENGTH_LONG)
-					.show();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-			
-			@Override
-			public void onPrepared(MediaPlayer mp) {
-				mp.start();	
-			}
-		});
+		mPlayer.prepareAsync();
+	}
 
-		// Prepare for playback
-		try {
-			mPlayer.prepareAsync();
-		} catch (IllegalStateException e) {
-			Toast.makeText(getApplicationContext(),
-					"You might not set the URI correctly!", Toast.LENGTH_LONG)
-					.show();
+	@Override
+	public void onCompletion(MediaPlayer arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean onError(MediaPlayer arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer mp) {
+		mp.start();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return liveStreamBinder;
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		mPlayer.stop();
+		mPlayer.release();
+		return false;
+	}
+
+	public class LiveStreamBinder extends Binder {
+		LiveStreamService getService() {
+			return LiveStreamService.this;
 		}
-		
-
 	}
 }
