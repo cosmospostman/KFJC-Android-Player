6b6b3db45f0b14e1a6e35f2ac68f9df0a73fb0b0
cosmospostman <buck68@gmail.com>
2016-01-14 14:23:02 +1100

    Rename default stream

--- src/org/kfjc/android/player/Constants.java
+++ src/org/kfjc/android/player/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final int CURRENT_TRACK_POLL_DELAY_MS = 30000;
 
     public static final String AVAILABLE_STREAMS_URL = "http://www.kfjc.org/netcast/streams.json";
-    public static final String FALLBACK_STREAM_NAME = "High/128k mp3";
+    public static final String FALLBACK_STREAM_NAME = "Default stream, 128k mp3";
     public static final String FALLBACK_STREAM_URL = "http://netcast6.kfjc.org:80/";
     public static final String FALLBACK_STREAM_JSON =
             String.format("[ { name:'%s', url:'%s' } ]", FALLBACK_STREAM_NAME, FALLBACK_STREAM_URL);
